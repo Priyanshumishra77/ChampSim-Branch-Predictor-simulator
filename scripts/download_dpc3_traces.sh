e47a0198a6a4c7489d3f6417c3bf3feab3c56472
Priyanshu Mishra <mpriyanshu236@gmail.com>
2024-08-21 12:02:02 +0530

    Update download_dpc3_traces.sh

--- scripts/download_dpc3_traces.sh
+++ scripts/download_dpc3_traces.sh
@@ -1,1 +1,8 @@
 
+#!/bin/bash
+
+mkdir -p $PWD/../dpc3_traces
+while read LINE
+do
+    wget -P $PWD/../dpc3_traces -c http://hpca23.cse.tamu.edu/champsim-traces/speccpu/$LINE
+done < dpc3_max_simpoint.txt
